9c12833296b538491d65820f9065b6df85d0d24d
Ziv Pizanti <zivp@wix.com>
2020-02-23 12:40:08 +0200

    wip

--- scripts/syncTS.sh
+++ scripts/syncTS.sh
@@ -4,11 +4,11 @@ set -e
 
 echo Checking prop type changes against d.ts files...
 
-if true;
+if [ \"$agentType\" = \"pullrequest\" ]
   then
     echo breaking build
     echo agentType: $agentType
     exit 1
   else
-    echo not breaking build
+    echo not breaking build - not a PR
 fi
