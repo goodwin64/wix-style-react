681b7d0d1aa320ecc1403a6d08f0b27e47296688
OmerB <omerb@wix.com>
2020-02-23 11:37:51 +0200

    Chore(syncTS): add initial script

---
+++ scripts/syncTS.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo Checking prop type changes against d.ts files...
+
+if true;
+  then
+    echo breaking build
+    echo agentType: $agentType
+    exit 1
+  else
+    echo not breaking build
+fi
