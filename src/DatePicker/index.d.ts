9a836c2ef7ce70d537c0bba8ee27a962fc5a9bbe
Egozi <egozi@wix.com>
2020-02-24 10:23:37 +0200

    <DatePicker/> - types

--- src/DatePicker/index.d.ts
+++ src/DatePicker/index.d.ts
@@ -2,23 +2,24 @@ import * as React from 'react';
 import {WixComponentProps} from "../BaseComponents/WixComponent";
 import {CalendarProps} from "../Calendar";
 
+export type DatePickerState = 'error' | 'warning' | 'loading';
 export interface DatePickerProps extends WixComponentProps, CalendarProps {
-  customInput?: React.ReactNode,
-  inputProps?: {},
-  dateFormat?: string | Function,
+  customInput?: React.ReactNode;
+  inputProps?: {};
+  dateFormat?: string | Function;
   locale?: string | {distanceInWords?: {}, format?: {},}
-  disabled?: boolean,
-  inputDataHook?: string,
-  calendarDataHook?: string,
-  placeholderText?: string,
-  rtl?: boolean,
-  value?: {},
-  isOpen?: boolean,
-  initialOpen?: boolean,
-  error?: boolean,
-  errorMessage?: React.ReactNode,
-  width?: number | string,
-  zIndex?: number,
+  disabled?: boolean;
+  inputDataHook?: string;
+  calendarDataHook?: string;
+  placeholderText?: string;
+  rtl?: boolean;
+  value?: {};
+  isOpen?: boolean;
+  initialOpen?: boolean;
+  status?: DatePickerState;
+  statusMessage?: React.ReactNode;
+  width?: number | string;
+  zIndex?: number;
 }
 
 export default class DatePicker extends React.PureComponent<DatePickerProps> {
